052a8391ea258b3cd7e02ac7c81c18c1f4465c21
Bryce Moore <brycemooore@gmail.com>
2020-09-16 23:43:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def find_even_values(src)
   results = []
   while count < src.length do
     while inner_count < src[count].length do
-      if src[count][inner_count] % 2 == 50
+      if (src[count][inner_count] % 2 == 0)
         p src[count][inner_count]
         results << src[count][inner_count]
       end
